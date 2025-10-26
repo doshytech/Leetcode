e9fa58ce3aa5da435f5138b395b2e9af1c3ae354
Dhairya Doshi <slayerdemon7386@gmail.com>
2025-10-26 19:24:26 +0530

    Time: 17 ms (30.19%) | Memory: 130.1 MB (10.83%) - LeetSync

--- 2169-simple-bank-system/simple-bank-system.cpp
+++ 2169-simple-bank-system/simple-bank-system.cpp
@@ -1,34 +1,34 @@
-vector<long long> p;
+unordered_map<int,long long> mp;
 int n;
 class Bank {
 public:
+    
     Bank(vector<long long>& balance) {
-        p = balance;
-        n = p.size();
+        n=balance.size();
+        for(int i=0;i<n;i++){
+            mp[i]=balance[i];
+        }
     }
+    
     bool transfer(int account1, int account2, long long money) {
-        bool tk = withdraw(account1,money);
-        if(tk){
-            if(deposit(account2,money))return true;
-            else{
-                deposit(account1,money);
-                return false;
-            }
-            // return true;
+        if(withdraw(account1,money)){
+            if(deposit(account2,money)) return true;
+            deposit(account1,money);
+            return false;
+            //  return true;
         }
         else return false;
     }
     
     bool deposit(int account, long long money) {
-        if(account<1||account>n)return false;
-        p[account-1]+=money;
+        if(account>n)return false;
+        mp[account-1]+=money;
         return true;
     }
     
     bool withdraw(int account, long long money) {
-        // if(account>p.size())return false;
-        if(account<1||account>n||p[account-1]<money)return false;
-        p[account-1]-=money;
+        if(account>n||mp[account-1]<money) return false;
+        mp[account-1]-=money;
         return true;
     }
 };
