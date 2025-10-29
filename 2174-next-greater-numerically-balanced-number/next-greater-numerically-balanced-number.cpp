073ac969396be76003a00639a5e0e4a87f28813d
Dhairya Doshi <slayerdemon7386@gmail.com>
2025-10-29 12:17:40 +0530

    Time: 2399 ms (5.00%) | Memory: 509.9 MB (5.09%) - LeetSync

---
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
@@ -0,0 +1,28 @@
+vector<int> ans;
+class Solution {
+public:
+    int nextBeautifulNumber(int n) {
+        if(ans.size()){
+            return *(upper_bound(ans.begin(),ans.end(),n));
+        }
+        else{
+            for(int i=1;i<2000000;i++){
+                unordered_map<int,int> ct;
+                int j = i;
+                while(j){
+                    ct[j%10]++;
+                    j/=10;
+                }
+                bool val = true;
+                for(auto &x: ct){
+                    if(x.first!=x.second){
+                        val = false;
+                        break;
+                    }
+                }
+                if(val)ans.push_back(i);
+            }
+            return nextBeautifulNumber(n);
+        }
+    }
+};
