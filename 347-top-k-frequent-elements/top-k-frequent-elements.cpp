2301a50cdda72292cd32eaaa3950f7dbb08a519f
Dhairya Doshi <slayerdemon7386@gmail.com>
2025-12-27 15:14:16 +0530

    Time: 3 ms (50.55%) | Memory: 18.3 MB (29.07%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        map<int,int> mp;
+        unordered_map<int,int> mp;
         for(auto &x: nums)mp[x]++;
         set<pair<int,int>> st;
-        for(auto &x: mp){
-            st.insert({-x.second,x.first});
-        }   
+        for(auto &x: mp)st.insert({-x.second,x.first});
         vector<int> res;
         auto it = st.begin();
         int i = 0;
