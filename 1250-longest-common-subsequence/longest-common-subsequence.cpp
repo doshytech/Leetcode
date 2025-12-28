305bc5498a62afcfccf474044d6ab66a5b586880
Dhairya Doshi <slayerdemon7386@gmail.com>
2025-12-28 09:21:53 +0530

    Time: 20 ms (83.33%) | Memory: 12.8 MB (87.31%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int n, m;
-    vector<vector<int>> dp;
+    // vector<vector<int>> dp;
     int longestCommonSubsequence(string a, string b) {
         n = a.size(), m = b.size();
+        int dp[1001][1001];
         // vector<int> p(m+1,0), c(m+1,0);
         // for(int i=1;i<=n;i++){
         //     for(int j=1;j<=m;j++){
@@ -17,14 +18,22 @@ public:
         //     p = c;
         // }
         // return p[m];
-        // memset(dp,-1,sizeof(dp));
-        dp.resize(n, vector<int> (m,-1));
-        return f(0,0,a,b);
+        memset(dp,0,sizeof(dp));
+        // dp.resize(n+1, vector<int> (m+1,0));
+        for(int i=n-1;i>-1;i--){
+            for(int j=m-1;j>-1;j--){
+                int not_take = max(dp[i+1][j],dp[i][j+1]);
+                int take = 0;
+                if(a[i]==b[j])take = 1+dp[i+1][j+1];
+                dp[i][j]=max(take, not_take);
+            }
+        }    
+        return dp[0][0];
     }
-    int f(int i, int j, string &a, string &b){
-        if(i==n||j==m)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(a[i]==b[j])return dp[i][j]=1+f(i+1,j+1,a,b);
-        else return dp[i][j]=max(f(i+1,j,a,b),f(i,j+1,a,b));
-    }
+    // int f(int i, int j, string &a, string &b){
+    //     if(i==n||j==m)return 0;
+    //     if(dp[i][j]!=-1)return dp[i][j];
+    //     if(a[i]==b[j])return dp[i][j]=1+f(i+1,j+1,a,b);
+    //     else return dp[i][j]=max(f(i+1,j,a,b),f(i,j+1,a,b));
+    // }
 };
