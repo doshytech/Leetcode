e28265a87d445050806b11037327473b3e634eed
Dhairya Doshi <slayerdemon7386@gmail.com>
2025-12-31 17:05:30 +0530

    Time: 3 ms (35.35%) | Memory: 77.2 MB (12.75%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,47 +1,30 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* list1, ListNode* list2) {
-        int carr = 0;
-        ListNode* dum = new ListNode();
-        ListNode* res = dum;
-        while(list1!=NULL&&list2!=NULL){
-            int val = list1->val+list2->val+carr;
-            dum->val = val%10;
-            carr = val/10; 
-            if(carr||list1->next||list2->next)dum->next = new ListNode();
-            dum = dum->next;
-            list1 = list1->next;
-            list2 = list2->next;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode();
+        ListNode* res = dummy;
+        int total = 0, carry = 0;
+
+        while (l1 || l2 || carry) {
+            total = carry;
+
+            if (l1) {
+                total += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                total += l2->val;
+                l2 = l2->next;
+            }
+
+            int num = total % 10;
+            carry = total / 10;
+            dummy->next = new ListNode(num);
+            dummy = dummy->next;
         }
-        while(list1!=NULL){
-            int val = list1->val+carr;
-            dum->val = val%10;
-            carr = val/10;
-            // dum->next = new ListNode();
-            if(carr||list1->next)dum->next = new ListNode();
-            dum = dum->next;
-            list1 = list1->next;
-        }
-        while(list2!=NULL){
-            int val = list2->val+carr;
-            dum->val = val%10;
-            carr = val/10;
-            if(carr||list2->next)dum->next = new ListNode();
-            dum = dum->next;
-            list2 = list2->next;
-        }
-        if(carr)dum->val+=carr;
-        // else dum = NULL;
-        return res;
+
+        ListNode* result = res->next;
+        delete res;
+        return result;         
     }
 };
